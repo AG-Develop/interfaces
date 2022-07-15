2b1c4b8e4f3a238114d91887507afb943f2bcde9
Krzysztof Kubanek <krzysztof.kubanek@gmail.com>
2022-07-15 14:56:32 +0200

    style: code cleanup

--- src/Json/SerializableInterface.php
+++ src/Json/SerializableInterface.php
@@ -2,7 +2,9 @@
 
 namespace AgDevelop\Interface\Json;
 
-interface SerializableInterface extends \JsonSerializable
+use JsonSerializable;
+
+interface SerializableInterface extends JsonSerializable
 {
 
 }
